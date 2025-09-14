e537d177e6be2e3e39eec12510ba2af56e81f413
Agraj Gautam <agrazgautam@users.noreply.github.com>
2025-09-14 18:54:29 -0500

    Adding read_layout function to read the region layout properly.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 config read_config();
 
+vector<vector<zone>> read_layout(const string region);
 
 
 config read_config()
@@ -68,6 +70,7 @@ config read_config()
         std::cerr << "Error reading the Refresh Rate value. Cause: " << e.what() << '\n';
     };
 
+    config_file.close();
     
 
     return configData;
@@ -75,6 +78,50 @@ config read_config()
 
 };
 
+// Reading the layout file in csv format.
+
+vector<vector<zone>> read_layout(const string region)
+{   
+    vector<vector<zone>> grid;
+
+    ifstream regionLayout (region);
+
+    if (!regionLayout.is_open())
+    {
+        string file_name;
+        cout << "Enter the Region Layout file: ";
+        getline(cin, file_name);
+
+        ifstream regionLayout(file_name);
+
+        if (!regionLayout.is_open()){
+            cout << "Error: The Region Layout file could not be loaded: " << file_name << endl;
+            return grid;
+        }
+    }
+
+    string line;
+
+    while (getline(regionLayout, line)) {
+        vector<zone> row;
+        string layout;
+        stringstream ss(line);
+
+        while (getline(ss, layout, ',')) {
+            zone cell;
+            cell.type = layout[0];   // First character of cell
+            cell.population = 0;
+            cell.pollution = 0;
+            row.push_back(cell);
+        }
+        grid.push_back(row);
+    }
+
+    regionLayout.close();
+
+    return grid;
+
+};
 
 
 
@@ -84,6 +131,13 @@ int main()
     config configData = read_config();
 
     if (configData.limit == -1 || configData.rate == -1){cout << "Error reading the config file." << endl; return -1;}
+
+    vector<vector<zone>> grid = read_layout(configData.layout);
+
+    if (grid.size() == 0) {cout <<"The Region Layout file could not be loaded properly." << endl; return -1;}
+    
+
+
 
 
 
