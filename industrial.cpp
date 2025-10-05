ab4d36e71e9398e201d3165c02f137b155cc1a6e
Agraj Gautam <agrazgautam@users.noreply.github.com>
2025-10-05 15:20:26 -0500

    feat: added functions to update the industrial type according to given condition.

--- industrial.cpp
+++ industrial.cpp
@@ -2,3 +2,160 @@
 #include "industrial.h"
 #include "commercial.h"
 #include "residential.h"
+
+/*
+Industrial
+
+    If a cell has a population of 0, is adjacent to a powerline in the current time step, and there are at least 2 available workers, that cell’s population will increase by 1 in the next time step and the available workers are assigned to that job
+    If a cell has a population of 0, is adjacent to at least one cell with a population of at least 1, and there are at least 2 available workers, that cell’s population will increase by 1 in the next time step and the available workers are assigned to that job
+    If a cell has a population of 1, is adjacent to at least two cells with a population of at least 1, and there are at least 2 available workers, that cell’s population will increase by 1 in the next time step and the available workers are assigned to that job
+    If a cell has a population of 2, is adjacent to at least four cells with a population of at least 2, and there are at least 2 available workers, that cell’s population will increase by 1 in the next time step and the available workers are assigned to that job
+    A cell produces pollution equal to its population, and pollution spreads to all adjacent cells at a rate of one less unit of pollution per cell away from the source
+    The industrial population provides goods to the commercial zones, at a rate of one good per population, but each good can only be sold at one commercial cell
+
+*/
+
+void updateIndustrial(vector<vector<zone>>& grid, bool& state)
+{   
+    state = false;
+
+    position largestCell = largest(grid, 'I');
+    position largestAdjacentCell = largestAdjacent(grid, 'I');
+    position cell;
+
+    
+    if (largestCell.row != -1)
+    {
+        updateIndustrialCondition(grid, largestCell, state);
+
+    }
+
+    if (state == true){return;}
+
+    if (largestAdjacentCell.row != -1)
+    {
+        updateIndustrialCondition(grid, largestAdjacentCell, state);
+    }
+
+    if (state == true){return;}
+
+
+    for (int row = 0; row < grid.size(); row++)
+    {
+        cell.row = row;
+
+        for (int col = 0; col < grid.at(row).size(); col++)
+        {   
+            
+            cell.col = col;
+
+            if (grid.at(row).at(col).type == 'R')
+            {
+                updateIndustrialCondition(grid, cell, state);
+
+                if (state == true){return;}
+
+            }
+
+        }
+    }
+    
+    return;
+
+
+}
+
+
+void updateIndustrialCondition(vector<vector<zone>>& grid, const position& cell, bool& state)
+{   
+    zone worker = available(grid, 'R');
+
+    int population = grid.at(cell.row).at(cell.col).population;
+
+    switch (population)
+
+    {
+    case 0:
+        if (adjacent(grid, cell, 'T').count > 0 && worker.count >= 2)
+        {
+            state = true;
+            grid.at(cell.row).at(cell.col).population = grid.at(cell.row).at(cell.col).population + 1;
+            grid.at(cell.row).at(cell.col).pollution = grid.at(cell.row).at(cell.col).pollution + 1;
+            grid.at(cell.row).at(cell.col).job = grid.at(cell.row).at(cell.col).job + 1;
+            grid.at(cell.row).at(cell.col).goods = grid.at(cell.row).at(cell.col).goods + 1;
+            grid.at(cell.row).at(cell.col).count = 1;
+
+            // update workers, pollution.
+
+            return;
+
+        }
+
+        else if (adjacent(grid, cell, 'I').count >= 1 && adjacent(grid, cell, 'I').population >= 1 && worker.count >= 2)
+        {
+            state = true;
+            grid.at(cell.row).at(cell.col).population = grid.at(cell.row).at(cell.col).population + 1;
+            grid.at(cell.row).at(cell.col).pollution = grid.at(cell.row).at(cell.col).pollution + 1;
+            grid.at(cell.row).at(cell.col).job = grid.at(cell.row).at(cell.col).job + 1;
+            grid.at(cell.row).at(cell.col).goods = grid.at(cell.row).at(cell.col).goods + 1;
+            grid.at(cell.row).at(cell.col).count = 1;
+
+            // update workers, pollution.
+
+            return;
+
+        }
+
+    break;
+
+
+    
+    case 1:
+
+        if (adjacent(grid, cell, 'I').count >= 2 && adjacent(grid, cell, 'I').population >= 1 && worker.count >= 2)
+        {
+            state = true;
+            grid.at(cell.row).at(cell.col).population = grid.at(cell.row).at(cell.col).population + 1;
+            grid.at(cell.row).at(cell.col).pollution = grid.at(cell.row).at(cell.col).pollution + 1;
+            grid.at(cell.row).at(cell.col).job = grid.at(cell.row).at(cell.col).job + 1;
+            grid.at(cell.row).at(cell.col).goods = grid.at(cell.row).at(cell.col).goods + 1;
+            grid.at(cell.row).at(cell.col).count = 1;
+
+            // update workers, pollution.
+
+            return;
+
+        }
+
+    break;
+
+
+    case 2:
+
+        if (adjacent(grid, cell, 'I').count >= 4 && adjacent(grid, cell, 'I').population >= 2 && worker.count >= 2)
+        {
+            state = true;
+            grid.at(cell.row).at(cell.col).population = grid.at(cell.row).at(cell.col).population + 1;
+            grid.at(cell.row).at(cell.col).pollution = grid.at(cell.row).at(cell.col).pollution + 1;
+            grid.at(cell.row).at(cell.col).job = grid.at(cell.row).at(cell.col).job + 1;
+            grid.at(cell.row).at(cell.col).goods = grid.at(cell.row).at(cell.col).goods + 1;
+            grid.at(cell.row).at(cell.col).count = 1;
+
+            // update workers, pollution.
+
+            return;
+
+        }
+    break;
+
+
+
+
+    default:
+    break;
+
+    }
+
+    return;
+
+}
--- industrial.h
+++ industrial.h
@@ -5,6 +5,9 @@
 #include "commercial.h"
 #include "residential.h"
 
+void updateIndustrial(vector<vector<zone>>& grid, bool& state); // uses the original grid to make changes through pass by reference
+
+void updateIndustrialCondition(vector<vector<zone>>& grid, const position& cell, bool& state);
 
 
 #endif
