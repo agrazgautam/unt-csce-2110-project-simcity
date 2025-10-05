a86e4e47a5eb887a68179067cd4e4ac0fc29ab36
Agraj Gautam <agrazgautam@users.noreply.github.com>
2025-10-05 15:04:05 -0500

    feat: adding industrial cpp and h files

---
+++ industrial.cpp
@@ -0,0 +1,4 @@
+#include "simulation.h"
+#include "industrial.h"
+#include "commercial.h"
+#include "residential.h"
---
+++ industrial.h
@@ -0,0 +1,10 @@
+#ifndef INDUSTRIAL
+#define INDUSTRIAL
+
+#include "simulation.h"
+#include "commercial.h"
+#include "residential.h"
+
+
+
+#endif
